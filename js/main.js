46dfcf0bb8fc24801f9826d1fb516019fd0e83fb
RAHUL-JAIN1351 <65061254+RAHUL-JAIN1351@users.noreply.github.com>
2020-07-23 22:00:13 +0530

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,1 +1,13 @@
-
+var i=0,cnt=0;
+       function f(){
+          document.getElementById('one').classList.toggle('do');
+     
+          if(cnt == 1){
+               document.getElementById('one').style.animation = "bulge 0.5s ease 0.5s infinite alternate";
+               cnt=0;
+          }
+          else if(cnt == 0){
+               document.getElementById('one').style.animation = "none";
+               cnt=1;
+          }
+       }
